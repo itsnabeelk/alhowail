8891dcc8c420ac9da839ed9c468d6865fe48b8af
itsnabeelk <itsnabeelk@gmail.com>
2024-01-30 22:55:26 +0300

    add

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,31 @@
 animejsPlugins.scrollContainer({
-    sectionSelector: '.section-scroll',
-    wrapperSelector: '.master',
-    duration: 1000,
-    easing: 'easeInOutQuad',
-    onBegin: (index, anime) => {
-      disappear()
-    },
-    onComplete: (index, anime) => {
-      appear(index);
-    } 
+  sectionSelector: '.section-scroll',
+  wrapperSelector: '.master',
+  duration: 1000,
+  easing: 'easeInOutQuad',
+  onBegin: (index, anime) => {
+    disappear()
+  },
+  onComplete: (index, anime) => {
+    appear(index);
+  } 
+})
+
+function appear(index) {
+  anime({
+    targets: `.section-scroll:nth-child(${index}) h1, p`,
+    opacity: [0, 1],
+    duration: anime.random(300, 600),
+    easing: 'easeInOutQuad'
   })
-  
-  function appear(index) {
-    anime({
-      targets: `.section-scroll:nth-child(${index}) h1`,
-      opacity: [0, 1],
-      duration: anime.random(300, 600),
-      easing: 'easeInOutQuad'
-    })
-  }
-  
-  function disappear() {
-    anime({
-      targets: `h1`,
-      opacity: [1, 0],
-      duration: anime.random(200, 400),
-      easing: 'easeInOutQuad'
-    })
-  }
+}
+function disappear() {
+  anime({
+    targets: `h1, p`,
+    opacity: [1, 0],
+    duration: anime.random(200, 400),
+    easing: 'easeInOutQuad'
+  })
+}
+
+
